efa2140f3053e971bd9a7e1e09511c08c22b436e
JohnSebastian-24 <juansebastianag24@gmail.com>
2020-11-05 13:33:27 -0500

    added data request

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import random
 
 chars ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKMNLROPQRSTUVWXYZ1234567890"
+
+while 1:
+    password_len = int(input("how many characters do you want in your password ?:"))
+    password_count = int(input("how many passwords would you like to generate ?"))
