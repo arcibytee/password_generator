14dc7f52e456f4aac6e26949fc0c69aa5bc07d9a
JohnSebastian-24 <juansebastianag24@gmail.com>
2020-11-05 16:33:27 -0500

    script completed

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import random
 
-chars ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKMNLROPQRSTUVWXYZ1234567890"
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKMNLROPQRSTUVWXYZ1234567890"
 
-while 1:
-    password_len = int(input("how many characters do you want in your password ?:"))
-    password_count = int(input("how many passwords would you like to generate ?"))
+while True:
+    password_len = int(input("How many characters do you want in your password?: "))
+    password_count = int(input("How many passwords would you like to generate?: "))
+
+    for _ in range(password_count):
+        password = ""
+        for _ in range(password_len):
+            password_char = random.choice(chars)
+            password += password_char
+
+        print("Here is your password: ", password)
