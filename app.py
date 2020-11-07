44fbfa6380d6a89c0af727f29990e4bff985add2
JohnSebastian-24 <juansebastianag24@gmail.com>
2020-11-07 18:33:27 -0500

    added option to select special characters

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 import random
 import string
 
-chars = string.ascii_letters + string.digits + string.punctuation
+def generate_password(password_len, use_special_chars=True):
+    chars = string.ascii_letters + string.digits
+    if use_special_chars:
+        chars += string.punctuation
+
+    password = "".join(random.choice(chars) for _ in range(password_len))
+    return password
 
 while True:
     password_len = int(input("How many characters do you want in your password?: "))
     password_count = int(input("How many passwords would you like to generate?: "))
-    
+    include_special_chars = input("Do you want to include special characters in the passwords? (yes/no): ").lower() == 'yes'
+
     save_passwords = input("Do you want to save the passwords to a file? (yes/no): ").lower() == 'yes'
 
     passwords = []
 
     for _ in range(password_count):
-        password = "".join(random.choice(chars) for _ in range(password_len))
+        password = generate_password(password_len, include_special_chars)
         passwords.append(password)
         print("Here is your password:", password)
 
